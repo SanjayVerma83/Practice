4e223298b825897df5e4bff0a6f85b42c6ba1017
Sanjay <skkverma83@gmail.com>
2026-09-04 13:25:02 +0530

    QA-104: Add user profile validation test

--- src/test/java/qumu/LoginTest.java
+++ src/test/java/qumu/LoginTest.java
@@ -2,10 +2,7 @@
 
 public class LoginTest {
     public void testValidLogin() {
-
         System.out.println("Running tests on Chrome Browser");
-
-        System.out.println("Running tests on Firefox Browser");
-
+        System.out.println("Running tests on Firefox Browser")
     }
 }
