c481ebd9193342cf6178a8f5a4219c148a868d88
Sanjay <skkverma83@gmail.com>
2026-09-04 12:08:22 +0530

    QA-101: Add LoginTest validation script

---
+++ src/test/java/qumu/LoginTest.java
@@ -0,0 +1,8 @@
+﻿package qumu;
+
+public class LoginTest {
+    // Simulated Selenium TestNG Login Script
+    public void testValidLogin() {
+        System.out.println("Executing Login Validation Test");
+    }
+}
