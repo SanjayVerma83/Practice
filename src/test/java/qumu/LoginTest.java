940d2f4662a8bab29e2b2ff455f4872bfb12c43b
Sanjay <skkverma83@gmail.com>
2026-09-04 13:27:06 +0530

    Fix: Resolve syntax error causing pipeline compilation failure

--- src/test/java/qumu/LoginTest.java
+++ src/test/java/qumu/LoginTest.java
@@ -3,6 +3,6 @@
 public class LoginTest {
     public void testValidLogin() {
         System.out.println("Running tests on Chrome Browser");
-        System.out.println("Running tests on Firefox Browser")
+        System.out.println("Running tests on Firefox Browser");
     }
 }
